5de320691a2174c6de6409879fe018e96c707017
dinno <t.dinno7@gmail.com>
2021-12-02 16:52:31 +0330

    Base

---
+++ assets/index.0aa08a30.js
@@ -0,0 +1,269 @@
+import{c as E,o as y,a as g,b as o,d as _,r as u,e as S,n as M,w as f,v as w,t as p,f as x,g as A,h as Y}from"./vendor.b89f38af.js";const B=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))e(n);new MutationObserver(n=>{for(const s of n)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&e(r)}).observe(document,{childList:!0,subtree:!0});function a(n){const s={};return n.integrity&&(s.integrity=n.integrity),n.referrerpolicy&&(s.referrerPolicy=n.referrerpolicy),n.crossorigin==="use-credentials"?s.credentials="include":n.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function e(n){if(n.ep)return;n.ep=!0;const s=a(n);fetch(n.href,s)}};B();const v=[{title:"Believer-(Ft-Lil-Wayne)",singer:"Imagine Dragons",musicSrc:"./musics/Imagine-Dragons-Believer-(Ft-Lil-Wayne).mp3",coverSrc:"./musics/Imagine-Dragons-Believer-(Ft-Lil-Wayne).jpeg",lyric:`
+    First things first
+    I'ma say all the words inside my head
+    I'm fired up and tired of the way that things have been, oh-ooh
+    The way that things have been, oh-ooh
+    Second thing second
+    Don't you tell me what you think that I could be
+    I'm the one at the sail, I'm the master of my sea, oh-ooh
+    The master of my sea, oh-ooh
+    I was broken from a young age
+    Taking my sulkin' to the masses
+    Writing my poems for the few
+    That look at me, took to me, shook to me, feelin' me
+    Singing from heartache from the pain
+    Taking my message from the veins
+    Speaking my lesson from the brain
+    Seeing the beauty through the...
+    Pain! You made me a, you made me a believer, believer
+    Pain! You break me down and build me up, believer, believer
+    Pain! Oh, let the bullets fly, oh, let them rain
+    My life, my love, my drive, it came from...
+    Pain! You made me a, you made me a believer, believer
+    First things first
+    Can you imagine what's about to happen?
+    It's Weezy the dragon, I link with the Dragons
+    And we gon' get ratchet, no need for imaginin'
+    This is what's happenin'
+    Second thing second, I reckon immaculate
+    Sound about accurate
+    I know that strength, it don't come without strategy
+    I know that sweet, it don't come without cavities
+    I know the passages come with some traffic
+    I start from the basement, end up in the attic
+    And third thing third
+    Whoever count me out, they simply can't count
+    Let's get mathematic, I'm up in this, huh
+    Is you a believer?
+    I get a unicorn out of a zebra
+    I wear my uniform like a tuxedo
+    This dragon don't hold his breath, don't need a breather
+    Love you Miss Cita, the son of a leader
+    I know that bloomin' don't come without rain
+    I know the losin' don't come without shame
+    I know the beauty don't come without, huh
+    Hold up, hold up, last thing last
+    I know that Tunechi don't come without Wayne
+    I know that losin' don't come without game
+    I know that beauty don't come without
+    Don't come without\u2026
+    Pain...
+    Pain! You made me a, you made me a believer, believer
+    Pain! You break me down and build me up, believer, believer
+    Pain! Oh, let the bullets fly, oh, let them rain
+    My life, my love, my drive, it came from...
+    Pain! You made me a, you made me a believer, believer
+    Last things last...
+    By the grace of the fire and the flames
+    You're the face of the future, the blood in my veins, oh-ooh
+    The blood in my veins, oh-ooh
+    But they never did, ever lived, ebbin' and flowin'
+    Inhibited, limited 'til it broke open and rained down
+    It rained down, like...
+    Pain! You made me a, you made me a believer, believer
+    Pain! You break me down and build me up, believer, believer
+    Pain! Oh, let the bullets fly, oh, let them rain
+    My life, my love, my drive, it came from...
+    Pain! You made me a, you made me a believer, believer`},{title:"Enemy",singer:"Imagine Dragons",musicSrc:"./musics/Imagine-Dragons-Enemy.mp3",coverSrc:"./musics/Imagine-Dragons-Enemy.jpeg",lyric:`
+    Look out for yourself
+    I wake up to the sounds of the silence that allows
+    For my mind to run around, with my ear up to the ground
+    I'm searching to behold the stories that are told
+    When my back is to the world that was smiling when I turned
+    Tell you you're the greatest
+    But once you turn, they hate us
+    Oh, the misery
+    Everybody wants to be my enemy
+    Spare the sympathy
+    Everybody wants to be my enemy-y-y-y-y
+    (Look out for yourself)
+    My enemy-y-y-y-y (Look, look, look, look)
+    (Look out for yourself)
+    But I'm ready
+    Your words up on th\u0435 wall as you're prayin' for my fall
+    And the laughter in th\u0435 halls and the names that I've been called
+    I stack it in my mind, and I'm waiting for the time
+    When I show you what it's like to be words spit in a mic
+    Tell you you're the greatest
+    But once you turn, they hate us (Huh)
+    Oh, the misery
+    Everybody wants to be my enemy
+    Spare the sympathy
+    Everybody wants to be my enemy-y-y-y-y (Look, look, look, look)
+    (Look out for yourself)
+    My enemy-y-y-y-y (Look, look, look, look; yeah)
+    (Look out for yourself)
+    Uh, look, okay
+    I'm hoping that somebody pray for me
+    I'm praying that somebody hope for me
+    I'm staying where nobody 'posed to be p-p-posted
+    Being a wreck of emotions
+    Ready to go whenever, just let me know
+    The road is long, so put the pedal into the floor
+    The enemy's on my trail, my energy unavailable
+    I'ma tell 'em, "Hasta luego"
+    They wanna plot on my trot to the top
+    I've been outta shape, thinkin' out the box, I'm an astronaut
+    I blasted off the planet rock to cause catastrophe
+    And it matters more because I had it not
+    Had I thought about wreaking havoc
+    On an opposition, kinda shockin' they wanted static
+    With precision, I'm automatic quarterback
+    I ain't talkin' sackin', pack it
+    Pack it up, I don't panic, batter-batter up
+    Who the baddest? It don't matter 'cause we at ya throat
+    Everybody wants to be my enemy
+    Spare the sympathy (Ah)
+    Everybody wants to be my enemy
+    Oh, the misery (Ah)
+    Everybody wants to be my enemy
+    Spare the sympathy (Ah)
+    Everybody wants to be my enemy
+    Pray it away, I swear I'll never be a saint, no way
+    My enemy
+    Pray it away, I swear I'll never be a saint
+    (Look out for yourself)`},{title:"Lonely",singer:"Imagine Dragons",musicSrc:"./musics/Imagine-Dragons-Lonely.mp3",coverSrc:"./musics/Imagine-Dragons-Lonely.jpeg",lyric:`
+    Sometimes, I can get a little
+    I can get a little lonely
+    Sometimes, I can get a little
+    I can get a little lonely
+    Some nights I get a little lonely
+    It's even when there's people all around me
+    Sometimes I get a little anxious
+    'Cause these pills don't work the way the doctor played it
+    I'm startin' to spin, needles and pins
+    Right on the brim, hand on a limb, takin' it in
+    Sleep it away, life in a day, and a day in a life
+    Mixing it up, checkin' it twice
+    Trying to breathe, starting to seize
+    Lights, camera, action and scene
+    Story of my life
+    Oh, love (Sometimes, I can get a little)
+    Have mercy (I can get a little lonely)
+    On me (Sometimes, I can get a little)
+    And keep me company (I can get a little lonely)
+    I said oh, love (Sometimes, I can get a little)
+    Have mercy (I can get a little lonely)
+    On me (Sometimes, I can get a little)
+    And keep me company (I can get a little lonely)
+    I said\u2014
+    Sometimes, I smile to keep things easy
+    I hide in corners, hope that no one sees me
+    So I'm playin' a role, ridin' the boat, stuck in a hole
+    Takin' a bow, lookin' so proud
+    Look at me now, talkin' about pills in my mouth
+    Under the tongue, isn't it fun?
+    Trying to breathe, starting to seize
+    Lights, camera, action and scene
+    Story of my life, oh-oh
+    Oh, love (Sometimes, I can get a little)
+    Have mercy (I can get a little lonely)
+    On me (Sometimes, I can get a little)
+    And keep me company (I can get a little lonely)
+    I said oh, love (Sometimes, I can get a little)
+    Have mercy (I can get a little lonely)
+    On me (Sometimes, I can get a little)
+    And keep me company (I can get a little lonely)
+    I said, "Oh, love" (Keep me company, I said, help keep me company, I said)
+    Have mercy (Help keep me company, I said, oh, love)
+    Oh, love, can you please have mercy on me? (Keep me company, I said, help keep me company, I said)
+    Can you keep me company? (Help keep me company, I said, help)
+    L-O-N-E-L-Why am I so lonely?
+    Oh, love (Sometimes, I can get a little)
+    Have mercy (I can get a little lonely)
+    (Keep me company, I said, help keep me company, I said)
+    On me (Sometimes, I can get a little)
+    And keep me company (I can get a little lonely)
+    (Help keep me company, I said, oh, love)
+    I said, oh, love (Sometimes, I can get a little)
+    Have mercy (I can get a little lonely)
+    (Keep me company, I said, help keep me company, I said)
+    On me (Sometimes, I can get a little)
+    And keep me company (I can get a little lonely)
+    (Help keep me company, I said, oh, love)
+    I said, oh\u2014`},{title:"Radioactive",singer:"Imagine Dragons",musicSrc:"./musics/Imagine-Dragons-Radioactive.mp3",coverSrc:"./musics/Imagine-Dragons-Radioactive.jpeg",lyric:`
+    I'm waking up to ash and dust
+    I wipe my brow and I sweat my rust
+    I'm breathing in the chemicals
+    I'm breaking in, shaping up
+    Checking out on the prison bus
+    This is it, the apocalypse, whoa
+    I'm waking up, I feel it in my bones
+    Enough to make my systems blow
+    Welcome to the new age, to the new age
+    Welcome to the new age, to the new age
+    Whoa, whoa, I'm radioactive, radioactive
+    Whoa, whoa, I'm radioactive, radioactive
+    I raise my flags, dye my clothes
+    It's a revolution, I suppose
+    We're painted red to fit right in
+    Whoa
+    I'm breaking in, shaping up
+    Then checking out on the prison bus
+    This is it, the apocalypse
+    Whoa
+    I'm waking up, I feel it in my bones
+    Enough to make my systems blow
+    Welcome to the new age, to the new age
+    Welcome to the new age, to the new age
+    Whoa, whoa, I'm radioactive, radioactive
+    Whoa, whoa, I'm radioactive, radioactive
+    All systems go, sun hasn't died
+    Deep in my bones, straight from inside
+    I'm waking up, I feel it in my bones
+    Enough to make my systems blow
+    Welcome to the new age, to the new age
+    Welcome to the new age, to the new age
+    Whoa, whoa, I'm radioactive, radioactive
+    Whoa, whoa, I'm radioactive, radioactive`},{title:"One Day",singer:"Imagine Dragons",musicSrc:"./musics/Imagine-Dragons-One-Day.mp3",coverSrc:"./musics/Imagine-Dragons-One-Day.jpeg",lyric:`
+    I know that one day
+    I'll be that one thing
+    That makes you happy
+    I know I keep you laughin'
+    Your sun on a cloudy day
+    The feel of a perfect wave
+    Is crashin' upon your face
+    Yeah, I know that one day, one day
+    Birds outside
+    Will sing me a song tonight
+    I know that it's right on time, time (Twee-da-da-da)
+    Life flies by
+    The moon and the midnight sky's
+    Guidin' me to your side, side
+    I know that one day
+    I'll be that one thing
+    That makes you happy
+    I know I keep you laughin'
+    Your sun on a cloudy day
+    The feel of a perfect wave
+    Is crashin' upon your face
+    Yeah, I know that one day, one day
+    Can we run away
+    To a place that children play
+    In the grassy field, field (Twee-da-da-da)
+    I know they're tellin' you things that you put in your head, head
+    Promise you that you are better off without them
+    I know that one day
+    I'll be that one thing
+    That makes you happy
+    I know I keep you laughin'
+    Your sun on a cloudy day
+    The feel of a perfect wave
+    Is crashin' upon your face
+    Yeah, I know that one day, one day
+    One (One, one, one) day
+    I'll be that one (One) thing
+    That makes you happy
+    I know I keep you laughin'
+    Your sun on a cloudy day (One, one)
+    The feel of a perfect wave
+    Is crashin' upon your face
+    Yeah, I know that one day, one day
+    Ooh, ooh-ooh-ooh-ooh
+    Ooh, ooh-ooh-ooh-ooh
+    Ooh, ooh-ooh-ooh-ooh
+    One day
+    Ooh, ooh-ooh-ooh-ooh
+    Ooh, ooh-ooh-ooh-ooh
+    Ooh, ooh-ooh-ooh-ooh
+    One day`}],T={play:{name:"play",iconSrc:"./icons/play.svg"},pause:{name:"pause",iconSrc:"./icons/pause.svg"},previus:{name:"previus",iconSrc:"./icons/previus.svg"},next:{name:"next",iconSrc:"./icons/next.svg"},volume:{name:"volume",iconSrc:"./icons/volume.svg"},mute:{name:"mute",iconSrc:"./icons/mute.svg"},lyric:{name:"mute",iconSrc:"./icons/lyric.svg"}};var b=(i,t)=>{const a=i.__vccOpts||i;for(const[e,n]of t)a[e]=n;return a};const H={name:"Controlers",props:["isPlaying","currentMusicTime","totalMusicTime"],setup(i){let t=E(()=>i.isPlaying);return{icons:T,isPlaying:t}}},j={class:"btns flex items-center justify-center space-x-6"},N={class:"prev cursor-pointer"},z=["src","alt"],F={class:"play-pause cursor-pointer"},R=["src","alt"],K=["src","alt"],V={class:"next cursor-pointer"},q=["src","alt"];function U(i,t,a,e,n,s){return y(),g("div",null,[o("div",j,[o("div",N,[o("img",{onClick:t[0]||(t[0]=r=>i.$emit("prevMusic")),class:"w-8 transition-all duration-300 transform hover:scale-125 bg-white/10 rounded-full p-1",src:e.icons.previus.iconSrc,alt:e.icons.previus.name},null,8,z)]),o("div",F,[e.isPlaying?_("",!0):(y(),g("img",{key:0,class:"playBtn w-14 transition-all duration-500 hover:rotate-[360deg] bg-white/10 rounded-full",onClick:t[1]||(t[1]=r=>i.$emit("playMusic")),src:e.icons.play.iconSrc,alt:e.icons.play.name},null,8,R)),e.isPlaying?(y(),g("img",{key:1,class:"pauseBtn w-14 transition-all duration-300 animate-pulse bg-white/10 rounded-full",onClick:t[2]||(t[2]=r=>i.$emit("pauseMusic")),src:e.icons.pause.iconSrc,alt:e.icons.pause.name},null,8,K)):_("",!0)]),o("div",V,[o("img",{onClick:t[3]||(t[3]=r=>i.$emit("nextMusic")),class:"w-8 transition-all duration-300 transform hover:scale-125 bg-white/10 rounded-full p-1",src:e.icons.next.iconSrc,alt:e.icons.next.name},null,8,q)])])])}var X=b(H,[["render",U]]);const G={name:"MusicPlayer",components:{Controlers:X},setup(){const i=u(null),t=u(0),a=u(null),e=u("--:--"),n=u("00:00"),s=u(!1),r=u(!1);u(!1),i.value=v[t.value],a.value=new Audio,a.value.src=i.value.musicSrc;function d(){let l=Math.floor(a.value.duration/60),m=Math.round(a.value.duration-l*60),c=Math.floor(a.value.currentTime/60),h=Math.round(a.value.currentTime-c*60);l<10&&(l="0"+l),m<10&&(m="0"+m),c<10&&(c="0"+c),h<10&&(h="0"+h),e.value=`${l}:${m}`,n.value=`${c}:${h}`}function P(l){let{duration:m,currentTime:c}=a.value,h=c/m*100,C=document.querySelector(".progress");C.style.width=`${h}%`}function O(l){let m=l.target.clientWidth+2,c=l.offsetX,{duration:h}=a.value;a.value.currentTime=c/m*h}const I=()=>{s.value=!0,a.value.play()},L=()=>{s.value=!1,a.value.pause()},W=()=>{t.value===0?t.value=v.length-1:t.value-=1,i.value=v[t.value],a.value.src=i.value.musicSrc,s.value&&I()},k=()=>{t.value===v.length-1?t.value=0:t.value+=1,i.value=v[t.value],a.value.src=i.value.musicSrc,s.value&&I()};a.value.addEventListener("loadedmetadata",l=>{d()}),a.value.addEventListener("timeupdate",l=>{d(),P()}),a.value.addEventListener("ended",l=>{k()});function D(){history.go(0)}return{currentMusic:i,currentMusicIndex:t,icons:T,audio:a,totalMusicTime:e,currentMusicTime:n,isPlaying:s,lyricShow:r,playMusic:I,pauseMusic:L,prevMusic:W,nextMusic:k,setProgress:O,test:D}}},J={class:"flex flex-col sm:flex-row items-center transition-all bg-transparent"},Q={class:"left-side relative bg-gradient-to-b from-gray-900 to-gray-800 z-10 w-80 h-[340px] px-5 py-5 rounded rounded-bl-2xl rounded-tr-2xl shadow-xl flex flex-col items-center"},Z=["src"],$={class:"volume cursor-pointer absolute transition-all left-1 top-1 w-[25px] hover:scale-110"},ee=["src"],te=["src"],oe={class:"title space-y-3 mb-6"},ae={class:"music-name"},ne={class:"text-2xl text-center text-white/90"},ie={class:"singer-name"},se={class:"text-base text-center text-white/80"},re=["src","alt"],le={class:"progress-bar mt-5 mb-2"},ce=o("div",{class:"progress h-[6px] rounded-[1px] bg-yellow-400 self-start"},null,-1),me=[ce],he={class:"times w-full mt-1 flex justify-between"},ue={class:"current-time text-xs text-white/90"},de={class:"total-time text-xs text-white/90"},ye={class:"btns"},ge={class:"Right-side w-56 h-80 overflow-hidden sm:rounded-r-xl z-0"},pe={class:"rounded-b-lg sm:rounded-b-none bg-gray-200 shadow-lg border-2 border-t-0 sm:border-t-2 sm:border-l-0 sm:border-r-0 border-gray-300 w-full h-52 sm:h-full overflow-auto"},ve={class:"px-4 py-2 text-justify text-[0.65rem] text-gray-900"},fe={class:"text-sm"};function we(i,t,a,e,n,s){const r=S("Controlers");return y(),g("div",J,[o("div",Q,[o("div",{onClick:t[0]||(t[0]=d=>e.lyricShow=!e.lyricShow),class:"lyric cursor-pointer absolute transition-all right-1 top-1 w-[25px] hover:scale-110"},[o("img",{class:M(["bg-white/10 opacity-80 hover:opacity-100 hover:-rotate-12 rounded-full p-1",{"animate-pulse":e.isPlaying&&!e.lyricShow}]),src:e.icons.lyric.iconSrc,alt:""},null,10,Z)]),o("div",$,[f(o("img",{onClick:t[1]||(t[1]=d=>e.audio.muted=!0),class:"bg-white/10 opacity-80 hover:opacity-100 hover:-rotate-12 rounded-full p-1",src:e.icons.volume.iconSrc,alt:""},null,8,ee),[[w,!e.audio.muted]]),f(o("img",{onClick:t[2]||(t[2]=d=>e.audio.muted=!1),class:"bg-white/10 opacity-80 hover:opacity-100 hover:-rotate-12 rounded-full p-1",src:e.icons.mute.iconSrc,alt:""},null,8,te),[[w,e.audio.muted]])]),o("div",oe,[o("div",ae,[o("h2",ne,p(e.currentMusic.title),1)]),o("div",ie,[o("h2",se,p(e.currentMusic.singer),1)])]),o("div",{class:M(["cover w-20 bg-transparent rounded-full",{"animate-spinner":e.isPlaying,"shadow-md":!e.isPlaying}])},[o("img",{class:"w-full transition-all hover:scale-105 rounded-full",src:e.currentMusic.coverSrc,alt:e.currentMusic.title},null,8,re)],2),o("div",le,[o("div",{onClick:t[3]||(t[3]=(...d)=>e.setProgress&&e.setProgress(...d)),class:"back-progress cursor-pointer w-52 h-2 rounded-sm bg-white/40 flex flex-col items-center justify-center"},me),o("div",he,[o("div",ue,p(e.currentMusicTime),1),o("div",de,p(e.totalMusicTime),1)])]),o("div",ye,[x(r,{isPlaying:e.isPlaying,currentMusicTime:e.currentMusicTime,totalMusicTime:e.totalMusicTime,onPlayMusic:e.playMusic,onPauseMusic:e.pauseMusic,onPrevMusic:e.prevMusic,onNextMusic:e.nextMusic},null,8,["isPlaying","currentMusicTime","totalMusicTime","onPlayMusic","onPauseMusic","onPrevMusic","onNextMusic"])])]),f(o("div",ge,[o("div",pe,[o("p",ve,[f(o("span",fe,"This music do not hava any lyric yet!",512),[[w,!e.currentMusic.lyric]]),A(" "+p(e.currentMusic.lyric),1)])])],512),[[w,e.lyricShow]])])}var Ie=b(G,[["render",we]]);const be={name:"App",components:{MusicPlayer:Ie}},ke={class:"w-screen h-screen flex justify-center items-center bg-gradient-to-br from-gray-900 to-gray-200"};function _e(i,t,a,e,n,s){const r=S("MusicPlayer");return y(),g("div",ke,[x(r,{style:{width:"500px"}})])}var Se=b(be,[["render",_e]]);Y(Se).mount("#app");
